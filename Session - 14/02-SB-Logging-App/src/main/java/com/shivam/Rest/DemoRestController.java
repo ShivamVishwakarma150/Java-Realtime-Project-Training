6be927f7b81260f6830cadd29807305bbb718f36
Shivam Vishwakarma <arvind.972176@gmail.com>
2023-05-12 19:00:38 +0530

    Logging in Spring Boot

---
+++ Session - 14/02-SB-Logging-App/src/main/java/com/shivam/Rest/DemoRestController.java
@@ -0,0 +1,28 @@
+package com.shivam.Rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoRestController {
+	
+	Logger logger = LoggerFactory.getLogger(DemoRestController.class);
+	
+	@GetMapping("/welcome")
+	public String getWelcomeMsg() {
+		
+		logger.info("getWelcomeMsg() execution started");
+		String msg = "Welcome to Mumbai";
+		
+		try {
+			int i = 10/0;
+		}catch(Exception e) {
+			logger.error("Exception Occured "+e.getMessage());
+		}
+		
+		logger.info("getWelcomeMsg() execution ended..");
+		return msg;
+	}
+}
